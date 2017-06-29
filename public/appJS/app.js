7f8e493fee48f4f7815a1298b72debb595e8888f
gagan <gagan@localhost.localdomain>
2017-06-29 15:49:19 +0530

    update

--- public/appJS/app.js
+++ public/appJS/app.js
@@ -1,18 +1,24 @@
-var sampleApp = angular.module('sampleApp', [
-    'ngRoute',
-    'appRoutes',
-    'MainCtrl',
-    'LoginCtrl',
-    'RegisterCtrl',
-    'simplePagination',
-    'angularSpinner',
-    'ngLoadingSpinner'
-]);
+(function() {
+    'use strict';
 
-sampleApp.run(['$location', '$rootScope', '$window', function ($location, $rootScope, $window) {
+    angular
+        .module('sampleApp', [
+            'ngRoute',
+            'appRoutes',
+            'MainCtrl',
+            'LoginCtrl',
+            'RegisterCtrl',
+            'simplePagination',
+            'angularSpinner',
+            'ngLoadingSpinner'
+        ])
+        .run(appRun);
 
-        $rootScope.$on('$routeChangeSuccess', function () {
-            $rootScope.showSection = $location.path() !== "/";
-        });
+    appRun.$inject = ['$location', '$rootScope', '$window'];
 
-    }]);
+    function appRun($location, $rootScope, $window) {
+      $rootScope.$on('$routeChangeSuccess', function () {
+          $rootScope.showSection = $location.path() !== "/";
+      });
+    }
+})();
